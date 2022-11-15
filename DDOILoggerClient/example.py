90a8b7a09cbac1c09dbf81eac41224ea6473a234
tylertucker202 <tylertucker202@gmail.com>
2022-11-14 16:18:56 -1000

    logger handler with example

---
+++ DDOILoggerClient/example.py
@@ -0,0 +1,32 @@
+from DDOILogger import ZMQHandler
+import os
+import logging
+from logging import StreamHandler, FileHandler
+import pdb
+
+def create_logger(subsystem, configLoc, author, progid, semid, fileName):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    zmq_log_handler = ZMQHandler(subsystem, configLoc, author, progid, semid)
+    ch = StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    fl = FileHandler(fileName)
+    fl.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(zmq_log_handler)
+    logger.addHandler(ch)
+    logger.addHandler(fl)
+    return logger
+
+if __name__=='__main__':
+    config_loc = os.path.join(os.getcwd(), 'logger_cfg.ini')
+    subsystem='MOSFIRE'
+    configLoc='./logger_cfg.ini'
+    author="ttucker"
+    progid="2022B"
+    semid="1234"
+    fileName = "test.log"
+
+    logger = create_logger(subsystem, configLoc, author, progid, semid, fileName)
+  
+    logger.warning({"msg": "logger handler test2", "level": "warning"})
