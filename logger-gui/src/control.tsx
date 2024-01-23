41af907242c3e704f2cce16e50760e9e7cc2a591
Tyler Tucker <tylertucker202@gmail.com>
2024-01-23 13:50:00 -1000

    restruct datetime picker

--- logger-gui/src/control.tsx
+++ logger-gui/src/control.tsx
@@ -1,29 +1,24 @@
 import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
-import Switch from '@mui/material/Switch'
 import TextField from '@mui/material/TextField'
 import React, { useEffect } from 'react'
 import { GetLogsArgs, log_functions } from './api/api_root'
 import { Log } from './log_view'
-import { BooleanParam, NumberParam, StringParam, useQueryParam, withDefault } from 'use-query-params'
+import { NumberParam, StringParam, useQueryParam, withDefault } from 'use-query-params'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import { Tooltip } from '@mui/material'
 
 interface Props {
     setLogs: Function
 }
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
-
 export const Control = (props: Props) => {
 
     const [n_logs, setNLogs] = useQueryParam('n_logs', withDefault(NumberParam, 100))
     const [minutes, setMinutes] = useQueryParam('log_minutes', withDefault(NumberParam, 10))
-    const [dateSwitch, setDateSwitchChange] = useQueryParam('date_switch', withDefault(BooleanParam, false))
     const [loggername, setLoggername] = useQueryParam('loggername', withDefault(StringParam, 'ddoi'))
     const [startdatetime, setStartdatetime] = useQueryParam<string | undefined>('startdatetime')
     const [enddatetime, setEnddatetime] = useQueryParam<string | undefined>('enddatetime')
@@ -41,10 +36,8 @@ export const Control = (props: Props) => {
         let logs: Log[] = []
         let params: GetLogsArgs = { n_logs: n_logs, loggername: ln }
         params['minutes'] = minutes > 0 ? minutes : undefined
-        if (dateSwitch) {
-            params['startdatetime'] = startdatetime ? startdatetime : undefined
-            params['enddatetime'] = enddatetime ? enddatetime : undefined
-        }
+        params['startdatetime'] = startdatetime ? startdatetime : undefined
+        params['enddatetime'] = enddatetime ? enddatetime : undefined
         logs = await log_functions.get_logs(params)
         const isString = typeof logs === 'string'
         console.log('logs', logs)
@@ -66,11 +59,6 @@ export const Control = (props: Props) => {
 
     const on_query_logs = () => {
         query_logs()
-    }
-
-    const on_date_switch_change = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log('date switch', event.target.checked)
-        setDateSwitchChange(event.target.checked)
     }
 
     const on_n_logs_change = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -103,20 +91,20 @@ export const Control = (props: Props) => {
     return (
         <Stack sx={{ marginBottom: '4px' }} direction="row" spacing={2}>
             <Tooltip title="get logs from n minutes ago (or from start date if defined)">
-            <TextField
-                label="minutes"
-                onChange={on_minutes_change}
-                value={minutes}
-                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-            />
+                <TextField
+                    label="minutes"
+                    onChange={on_minutes_change}
+                    value={minutes}
+                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                />
             </Tooltip>
             <Tooltip title="set limit to n logs">
-            <TextField
-                label="number of logs"
-                onChange={on_n_logs_change}
-                value={n_logs}
-                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-            />
+                <TextField
+                    label="number of logs"
+                    onChange={on_n_logs_change}
+                    value={n_logs}
+                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                />
             </Tooltip>
             <TextField
                 label="logger name (ddoi or koa)"
@@ -124,26 +112,28 @@ export const Control = (props: Props) => {
                 value={loggername}
             />
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <FormControlLabel control={
-                    <Switch {...label}
-                        checked={dateSwitch}
-                        onChange={on_date_switch_change}
-                    />
-                } label={'toggle on to include date(s)'} />
-                {
-                    dateSwitch && <React.Fragment>
-                        <DateTimePicker
-                            label='start time'
-                            onChange={(value: dayjs.Dayjs | null) => value && dt_changed(value, 'startdatetime')}
-                            value={startdatetime ? dayjs(startdatetime as string) : undefined}
-                            views={['year', 'day', 'hours', 'minutes', 'seconds']} />
-                        <DateTimePicker
-                            label='end time'
-                            onChange={(value: dayjs.Dayjs | null) => value && dt_changed(value, 'enddatetime')}
-                            value={enddatetime ? dayjs(enddatetime as string) : undefined}
-                            views={['year', 'day', 'hours', 'minutes', 'seconds']} />
-                    </React.Fragment>
-                }
+                <React.Fragment>
+                    <DateTimePicker
+                        label='start time'
+                        slotProps={{
+                            actionBar: {
+                                actions: ['clear'],
+                            },
+                        }}
+                        onChange={(value: dayjs.Dayjs | null) => value && dt_changed(value, 'startdatetime')}
+                        value={startdatetime ? dayjs(startdatetime as string) : undefined}
+                        views={['year', 'day', 'hours', 'minutes', 'seconds']} />
+                    <DateTimePicker
+                        slotProps={{
+                            actionBar: {
+                                actions: ['clear'],
+                            },
+                        }}
+                        label='end time'
+                        onChange={(value: dayjs.Dayjs | null) => value && dt_changed(value, 'enddatetime')}
+                        value={enddatetime ? dayjs(enddatetime as string) : undefined}
+                        views={['year', 'day', 'hours', 'minutes', 'seconds']} />
+                </React.Fragment>
             </LocalizationProvider>
             <Button variant={'contained'}
                 onClick={on_query_logs}
